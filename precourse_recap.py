8342fc021d901b4b51661b2da630266b9d4f7da4
Andrew Scott Wise <andrewwise@Andrews-MacBook-Pro.local>
2022-06-20 16:17:05 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,11 @@
+day = 8
+greeting = "Hello "
+name = "Andrew"
+positive_response = " well done on a great day"
+negative_response = " reflect on what went wrong today"
+
+if day >= 5:
+    print(greeting + name + positive_response)
+
+else:
+     print(greeting + name + negative_response)
